870110f4b6af3112eea384d036401012d74a1408
jinaybothra <jinaydeep2001@gmail.com>
2025-10-19 03:35:24 +0530

    modified login logic

--- backend/index.js
+++ backend/index.js
@@ -130,7 +130,7 @@ app.post('/auth/login', async (req, res) => {
   const user = await User.findOne({ email });
   if (!user) return res.status(400).json({ message: 'Invalid credentials' });
 
-  const isMatch = password === user.password;
+  const isMatch = bcrypt.compare(password, user.password);
   if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
 
   const token = jwt.sign({ id: user._id }, process.env.MONGO_URI, { expiresIn: '1h' });
